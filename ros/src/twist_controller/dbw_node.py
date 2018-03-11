f06d6c00c6041537344342392c7a773893386537
student <eng.mohamed.salaheldin@gmail.com>
2018-03-11 06:00:56 -0700

    full solution initial commit

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -4,7 +4,6 @@ import rospy
 from std_msgs.msg import Bool
 from dbw_mkz_msgs.msg import ThrottleCmd, SteeringCmd, BrakeCmd, SteeringReport
 from geometry_msgs.msg import TwistStamped
-import math
 
 from twist_controller import Controller
 
@@ -35,43 +34,57 @@ class DBWNode(object):
     def __init__(self):
         rospy.init_node('dbw_node')
 
-        vehicle_mass = rospy.get_param('~vehicle_mass', 1736.35)
-        fuel_capacity = rospy.get_param('~fuel_capacity', 13.5)
-        brake_deadband = rospy.get_param('~brake_deadband', .1)
-        decel_limit = rospy.get_param('~decel_limit', -5)
-        accel_limit = rospy.get_param('~accel_limit', 1.)
-        wheel_radius = rospy.get_param('~wheel_radius', 0.2413)
-        wheel_base = rospy.get_param('~wheel_base', 2.8498)
-        steer_ratio = rospy.get_param('~steer_ratio', 14.8)
-        max_lat_accel = rospy.get_param('~max_lat_accel', 3.)
-        max_steer_angle = rospy.get_param('~max_steer_angle', 8.)
+        self.linear_target = None
+        self.angular_target = None
+        self.linear_current = None
 
-        self.steer_pub = rospy.Publisher('/vehicle/steering_cmd',
-                                         SteeringCmd, queue_size=1)
-        self.throttle_pub = rospy.Publisher('/vehicle/throttle_cmd',
-                                            ThrottleCmd, queue_size=1)
-        self.brake_pub = rospy.Publisher('/vehicle/brake_cmd',
-                                         BrakeCmd, queue_size=1)
+        # debug for veer issue
+        self.last_required_vel_angular = 0.0
+        self.count_required_vel_angular = 0
 
-        # TODO: Create `Controller` object
-        # self.controller = Controller(<Arguments you wish to provide>)
+        # default to drive-by-wire not enabled - will pick this up from the topic...
+        self.dbw_enabled = False
+        self.sampling_rate = 50.0 # 50Hz
 
-        # TODO: Subscribe to all the topics you need to
+        self.controller = Controller(sampling_rate=self.sampling_rate,
+                                     decel_limit=rospy.get_param('~decel_limit', -5),
+                                     accel_limit=rospy.get_param('~accel_limit', 1.),
+                                     brake_deadband=rospy.get_param('~brake_deadband', .1),
+                                     vehicle_mass=rospy.get_param('~vehicle_mass', 1736.35),
+                                     fuel_capacity=rospy.get_param('~fuel_capacity', 13.5),
+                                     wheel_radius=rospy.get_param('~wheel_radius', 0.2413),
+                                     wheel_base=rospy.get_param('~wheel_base', 2.8498),
+                                     steer_ratio=rospy.get_param('~steer_ratio', 14.8),
+                                     max_lat_accel=rospy.get_param('~max_lat_accel', 3.),
+                                     max_steer_angle=rospy.get_param('~max_steer_angle', 8.))
+
+        self.steer_pub = rospy.Publisher('/vehicle/steering_cmd', SteeringCmd, queue_size=1)
+        self.throttle_pub = rospy.Publisher('/vehicle/throttle_cmd', ThrottleCmd, queue_size=1)
+        self.brake_pub = rospy.Publisher('/vehicle/brake_cmd', BrakeCmd, queue_size=1)
+
+        #Subscribe to all the topics you need to
+        rospy.Subscriber('/vehicle/dbw_enabled', Bool, self.callback_dbw_enabled)
+        # /current_velocity topic gives the current speed of the vehicle
+        rospy.Subscriber('/current_velocity', TwistStamped, self.callback_current_velocity)
+        # /twist_cmd topic is the output from the vehicles waypoint controller
+        # As implemented in waypoint_follower / pure_pursuit given code
+        rospy.Subscriber('/twist_cmd', TwistStamped, self.callback_twist_cmd)
 
         self.loop()
 
     def loop(self):
-        rate = rospy.Rate(50) # 50Hz
+        rate = rospy.Rate(self.sampling_rate) # Was 50Hz
         while not rospy.is_shutdown():
-            # TODO: Get predicted throttle, brake, and steering using `twist_controller`
-            # You should only publish the control commands if dbw is enabled
-            # throttle, brake, steering = self.controller.control(<proposed linear velocity>,
-            #                                                     <proposed angular velocity>,
-            #                                                     <current linear velocity>,
-            #                                                     <dbw status>,
-            #                                                     <any other argument you need>)
-            # if <dbw is enabled>:
-            #   self.publish(throttle, brake, steer)
+            if self.linear_current is None or self.linear_target is None \
+            or not self.dbw_enabled:
+                continue
+
+            throttle, brake, steer = self.controller.control(
+                self.linear_target,
+                self.angular_target,
+                self.linear_current)
+
+            self.publish(throttle, brake, steer)
             rate.sleep()
 
     def publish(self, throttle, brake, steer):
@@ -84,6 +97,7 @@ class DBWNode(object):
         scmd = SteeringCmd()
         scmd.enable = True
         scmd.steering_wheel_angle_cmd = steer
+        #rospy.loginfo('TwistController: Steering = ' + str(steer))
         self.steer_pub.publish(scmd)
 
         bcmd = BrakeCmd()
@@ -92,6 +106,29 @@ class DBWNode(object):
         bcmd.pedal_cmd = brake
         self.brake_pub.publish(bcmd)
 
+    def callback_dbw_enabled(self, msg):
+        # check if drive-by-wire is enabled (i.e. the car is not in manual mode)
+        self.dbw_enabled = msg.data
+        rospy.loginfo('TwistController: dbw_enabled = ' + str(self.dbw_enabled))
+
+        if not self.dbw_enabled:
+            self.controller.reset()
+
+    def callback_current_velocity(self, msg):
+        # store the current velocity TwistStamped message
+        self.linear_current = msg.twist.linear.x
+
+    def callback_twist_cmd(self, msg):
+        # store the received TwistStamped message from the waypoint follower node
+        self.linear_target = msg.twist.linear.x
+        self.angular_target = msg.twist.angular.z
+
+        # debugging veering issue
+        #if self.angular_target <> self.last_required_vel_angular:
+       #     rospy.loginfo('Veer: received new angular velocity required : ' + str(self.angular_target) + ', count = ' + str(self.count_required_vel_angular))
+        #    self.count_required_vel_angular = 0
+       # self.last_required_vel_angular = self.angular_target
+       # self.count_required_vel_angular += 1
 
 if __name__ == '__main__':
     DBWNode()
--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -3,8 +3,11 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 from styx_msgs.msg import Lane, Waypoint
+from std_msgs.msg import Int32
 
 import math
+import tf.transformations as tf
+import numpy as np
 
 '''
 This node will publish waypoints from the car's current position to some `x` distance ahead.
@@ -21,7 +24,7 @@ as well as to verify your TL classifier.
 TODO (for Yousuf and Aaron): Stopline location for each traffic light.
 '''
 
-LOOKAHEAD_WPS = 200 # Number of waypoints we will publish. You can change this number
+LOOKAHEAD_WPS = 20 # Number of waypoints we will publish. You can change this number
 
 
 class WaypointUpdater(object):
@@ -30,27 +33,182 @@ class WaypointUpdater(object):
 
         rospy.Subscriber('/current_pose', PoseStamped, self.pose_cb)
         rospy.Subscriber('/base_waypoints', Lane, self.waypoints_cb)
-
         # TODO: Add a subscriber for /traffic_waypoint and /obstacle_waypoint below
-
+	rospy.Subscriber('/traffic_waypoint', Int32, self.traffic_cb)
 
         self.final_waypoints_pub = rospy.Publisher('final_waypoints', Lane, queue_size=1)
 
         # TODO: Add other member variables you need below
+        self.base_wps=[]
+	self.final_wps = []
+	self.mph2mps = .447
+	self.is_wps_available = False
+	self.is_pos_update_available = False
+	self.max_speed = 35. * self.mph2mps 
+	self.min_speed = 20. * self.mph2mps 
+	self.speed = 0. # start at standstill
+	self.current_idx = -1
+	#self.next_red_light_wp_idx = 750
+	self.next_red_light_wp_idx = -1
+	self.start_deceleration = False
+	self.standstill = False
+	#self.flag = False
+        self.schedule(10)
 
-        rospy.spin()
+    def schedule(self,freq):
+	rate = rospy.Rate(freq)	
+	#temp = 0
+	while rospy.is_shutdown() != True:
+	    if self.is_wps_available == True and self.is_pos_update_available == True:
+	    	self.final_wps = self.wps_update()
+	        self.is_pos_update_available = False
+	    self.publish(self.final_wps)
+	    rate.sleep()
+
+	    #if self.flag == True:
+		#temp += 1
+		#if temp > 50:
+		 #   self.next_red_light_wp_idx = -1
+
+    def update_velocity_2(self,fwps):
+	veh_yaw = tf.euler_from_quaternion([self.pos.orientation.x,
+					  self.pos.orientation.y,
+					  self.pos.orientation.z,
+					  self.pos.orientation.w])
+	wp_yaw = tf.euler_from_quaternion([fwps[0].pose.pose.orientation.x,
+					  fwps[0].pose.pose.orientation.y,
+					  fwps[0].pose.pose.orientation.z,
+					  fwps[0].pose.pose.orientation.w])
+
+
+	
+	if math.fabs(self.pos.position.y - fwps[0].pose.pose.position.y) <= .5 and math.fabs(veh_yaw[2] - wp_yaw[2]) <= np.pi/80.:
+	#self.speed += self.mph2mps
+	    self.speed += 1.
+	else:
+	#self.speed -= self.mph2mps
+	    self.speed -= 1.
+	
+	if self.speed > self.max_speed:
+	    self.speed = self.max_speed
+	if self.speed < self.min_speed:
+	    self.speed = self.min_speed
+
+	for wp in fwps:
+	    wp.twist.twist.linear.x = self.speed
+	return fwps
+    def update_velocity(self,fwps):
+	if self.next_red_light_wp_idx > 0:
+	    dist = 1000000
+	    dist = self.distance(self.base_wps,self.current_idx,self.next_red_light_wp_idx)
+	    if dist <= (self.speed*1.5):
+		if self.start_deceleration == False:
+		    self.max_speed = self.speed
+		self.start_deceleration = True
+	else:
+	    self.start_deceleration = False
+	
+	if self.start_deceleration == True:
+	    #self.speed -= self.mph2mps	    
+	    self.speed -= .5
+	    if self.speed <= -1.:
+		#self.flag = True
+	        self.speed = -1.
+		self.standstill = True
+	    for wp in fwps:
+	        wp.twist.twist.linear.x = self.speed
+        elif self.standstill == True:
+            #veh_yaw = tf.euler_from_quaternion([self.pos.orientation.x,
+		#			  self.pos.orientation.y,
+		#			  self.pos.orientation.z,
+		#			  self.pos.orientation.w])
+	    #wp_yaw = tf.euler_from_quaternion([fwps[0].pose.pose.orientation.x,
+		#			  fwps[0].pose.pose.orientation.y,
+		#			  fwps[0].pose.pose.orientation.z,
+		#			  fwps[0].pose.pose.orientation.w])
+
+	    #self.max_speed = fwps[0].twist.twist.linear.x
+	    self.speed += 1.
+	    print(self.max_speed)
+	    #self.min_speed = self.max_speed - 3.
+	
+	    #if math.fabs(self.pos.position.y - fwps[0].pose.pose.position.y) <= 1. and math.fabs(veh_yaw[2] - wp_yaw[2]) <= np.pi/40.:
+	        #self.speed += self.mph2mps
+		#self.speed += 1.
+	    #else:
+	        #self.speed -= self.mph2mps
+		#self.speed -= 1.
+	
+	    if self.speed >= self.max_speed:
+	        self.speed = self.max_speed
+		self.standstill == False
+	    #if self.speed < self.min_speed:
+	    #    self.speed = self.min_speed
+
+	    for wp in fwps:
+	        wp.twist.twist.linear.x = self.speed
+	else:
+	    self.speed = fwps[0].twist.twist.linear.x
+
+	#print("speed = " + str(fwps[0].twist.twist.linear.x))
+
+	return fwps
+
+    def wps_update(self):
+        final_wps = []
+	#print(((self.pos.position.x)))
+	#is_point_located = False
+	#if self.pos.position.x < 100.:
+	#    self.current_idx = 0
+	if self.current_idx < 0:
+	    for idx in range(len(self.base_wps)):
+	        if(self.pos.position.x >= self.base_wps[(self.current_idx+idx)%len(self.base_wps)].pose.pose.position.x and
+		       self.pos.position.x <= self.base_wps[(self.current_idx+idx+1)%len(self.base_wps)].pose.pose.position.x) or (
+			(self.pos.position.x <= self.base_wps[(self.current_idx+idx)%len(self.base_wps)].pose.pose.position.x and
+		        self.pos.position.x >= self.base_wps[(self.current_idx+idx+1)%len(self.base_wps)].pose.pose.position.x)) and (
+			math.fabs(self.pos.position.y - self.base_wps[(idx)%len(self.base_wps)].pose.pose.position.y) <= 1. or (
+			math.fabs(self.pos.position.y - self.base_wps[(idx+1)%len(self.base_wps)].pose.pose.position.y) <= 1.)):
+		    self.current_idx = idx + 1
+                    break
+	else:
+	    for idx in range(self.current_idx,self.current_idx + 200):
+		if(self.pos.position.x >= self.base_wps[(idx)%len(self.base_wps)].pose.pose.position.x and
+		       self.pos.position.x <= self.base_wps[(idx+1)%len(self.base_wps)].pose.pose.position.x) or (
+			(self.pos.position.x <= self.base_wps[(idx)%len(self.base_wps)].pose.pose.position.x and
+		        self.pos.position.x >= self.base_wps[(idx+1)%len(self.base_wps)].pose.pose.position.x)):
+		    self.current_idx = (idx + 1)%len(self.base_wps)
+		    print(self.current_idx)
+		    #print(self.base_wps[self.current_idx].pose.pose.position.x)
+	            break
+
+        for idx in range(0,LOOKAHEAD_WPS):
+	    #temp = self.base_wps[(idx + self.current_idx)%len(self.base_wps)]
+	    #temp.twist.twist.linear.x = self.max_speed
+            final_wps.append(self.base_wps[(idx + self.current_idx)%len(self.base_wps)])
+	    #final_wps.append(temp)
+
+	if len(final_wps) > 0:
+	    final_wps = self.update_velocity(final_wps)
+	#print("wp = " + str(final_wps[0].pose.pose.position.x))
+	#print(self.current_idx)
+	return final_wps
 
     def pose_cb(self, msg):
         # TODO: Implement
-        pass
+	self.is_pos_update_available = True
+        self.pos = msg.pose
+
+	#self.final_wps = self.wps_update()
 
     def waypoints_cb(self, waypoints):
         # TODO: Implement
-        pass
-
+        self.base_wps = waypoints.waypoints
+	self.is_wps_available = True
+       
     def traffic_cb(self, msg):
         # TODO: Callback for /traffic_waypoint message. Implement
-        pass
+	self.next_red_light_wp_idx = msg.data
+        print(msg.data)
 
     def obstacle_cb(self, msg):
         # TODO: Callback for /obstacle_waypoint message. We will implement it later
@@ -70,6 +228,12 @@ class WaypointUpdater(object):
             wp1 = i
         return dist
 
+    def publish(self,waypoints):
+        lane = Lane()
+        lane.header.frame_id = '/world'
+        lane.header.stamp = rospy.Time(0)
+        lane.waypoints = waypoints
+        self.final_waypoints_pub.publish(lane)
 
 if __name__ == '__main__':
     try:
